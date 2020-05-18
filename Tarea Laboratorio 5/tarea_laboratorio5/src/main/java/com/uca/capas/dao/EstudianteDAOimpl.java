101b57a96c51567f50d0036946b87fd457b194ef
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-05-18 09:18:15 -0600

    Finish Tarea labo 5

---
+++ Tarea Laboratorio 5/tarea_laboratorio5/src/main/java/com/uca/capas/dao/EstudianteDAOimpl.java
@@ -0,0 +1,36 @@
+package com.uca.capas.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.uca.capas.domain.Estudiante;
+
+@Repository
+public class EstudianteDAOimpl implements EstudianteDAO {
+
+	@PersistenceContext(unitName="capas")
+	private EntityManager entityManager;
+
+	@Override
+	public List<Estudiante> findAll() throws DataAccessException {
+		StringBuffer sb = new StringBuffer();
+		sb.append("select * from public.estudiante");
+		Query query = entityManager.createNativeQuery(sb.toString(), Estudiante.class);
+		List<Estudiante> resulset = query.getResultList();
+		return resulset;
+	}
+
+	@Override
+	@Transactional
+	public void insert(Estudiante estudiante) throws DataAccessException {
+		entityManager.persist(estudiante);		
+	}
+	
+}
